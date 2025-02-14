7bad985a5bb563b23374d5eb7c061f5a0e78c616
Diogocavalheirojustulin <diogojustulin992@gmail.com>
2025-02-14 11:20:22 -0300

    oi

---
+++ Models/Etapas.cs
@@ -0,0 +1,23 @@
+﻿using PrimeiroMVC.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace SGE.Models
+
+{ 
+   [Table("Etapas")]
+    public class Etapas
+    {
+    [Column("Id")]
+    [Display(Name = "Id das Materias")]
+    public int Id { get; set; }
+
+    [Column("titulo")]
+    [Display(Name = "Título da Materias")]
+    public string Titulo { get; set; } = string.Empty;
+
+    [Column("Ano")]
+    [Display(Name = "Ano")]
+    public int Ano { get; set; }
+    }
+}
---
+++ Models/Materias.cs
@@ -0,0 +1,25 @@
+﻿using PrimeiroMVC.Models;
+using SGE.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SGE.Models
+{
+    [Table("Materias")]
+    public class Materias
+    {
+        [Column("Id")]
+        [Display(Name = "Id das Materias")]
+        public int Id { get; set; }
+
+        [Column("titulo")]
+        [Display(Name = "Título da Materias")]
+        public string Titulo { get; set; } = string.Empty;
+
+        [ForeignKey("Professor_FK")]
+        [Display(Name = "Professores")]
+        public int ProfessoresId { get; set; }
+        public Professores? Professores { get; set; }
+
+    }
+}
